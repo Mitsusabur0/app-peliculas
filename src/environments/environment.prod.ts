0e5cdb92214537e2625bdbf224f4722183be13da
mitsusaburo <bschindlerm@gmail.com>
2023-12-09 20:06:21 -0300

    Editar carpeta android y gradle

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "d0be9cd3427f72bdc95c2a4dc9294e02",
+  baseUrl: "https://api.themoviedb.org/3",
+  images: "http://image.tmdb.org/t/p",
 };
