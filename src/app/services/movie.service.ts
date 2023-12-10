8976ee6873bdf34e60982fa0db55d66d2a06be1a
mitsusaburo <bschindlerm@gmail.com>
2023-12-10 14:18:48 -0300

    Agregar imports a environment.ts

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/theme/environments/environment';
 
 export interface ApiResult {
   page: number;
